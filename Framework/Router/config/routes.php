37208bec790745347f76a486ac39139882deac81
Karlova Lesia <karlovalesa06@gmail.com>
2021-07-19 13:05:53 +0300

    Create array routes

--- Framework/Router/config/routes.php
+++ Framework/Router/config/routes.php
@@ -1,1 +1,10 @@
 <?php
+return [
+    'dresses' => 'dresses/list', //actionList in DressesController
+    'blouses' => 'blouses/list',
+    'tops' => 'tops/list',
+    't-shirts' => 't-shirts/list',
+    'shorts' => 'shorts/list',
+    'skirts' => 'skirts/list',
+    'jeans' => 'jeans/list'
+];
