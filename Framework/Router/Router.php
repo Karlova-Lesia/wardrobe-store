f1c022aa082cc4a7a3e3f9266683e6116f64882a
Karlova Lesia <karlovalesa06@gmail.com>
2021-07-19 13:09:28 +0300

    Create class Router for reading routes

--- Framework/Router/Router.php
+++ Framework/Router/Router.php
@@ -1,7 +1,46 @@
-<?php
+<?php /** @noinspection PhpUndefinedVariableInspection */
+/** @noinspection PhpIncludeInspection */
+
+/** @noinspection PhpPropertyOnlyWrittenInspection */
 
 
 class Router
 {
+    private $routes;
 
+    public function __construct()
+    {
+        $routesPath = ROOT.'/Framework/Router/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+
+    private function getURI(): string
+    {
+        return (!empty($_SERVER['REQUEST_URI'])) ? trim($_SERVER['REQUEST_URI'], '/') : false;
+    }
+
+    public function run()
+    {
+        $uri = $this->getURI();
+
+        foreach ($this->routes as $uriPattern => $path) {
+            if (preg_match("~$uriPattern~", $uri)) {
+                $segments = explode('/', $path);
+                $controllerName = array_shift($segments).'Controller';
+                $controllerName = ucfirst($controllerName);
+                $actionName = 'action'.ucfirst(array_shift($segments));
+
+                $controllerFile = ROOT.'/App/Controller/'.$controllerName.'.php';
+                if (file_exists($controllerFile)) {
+                    include_once($controllerFile);
+                }
+
+                $controllerObject = new $controllerName;
+                $result = $controllerObject->$actionName();
+                if ($result != null) {
+                    break;
+                }
+            }
+        }
+    }
 }
