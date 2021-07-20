225a3f300fd3dcab28ed4aa634e808c1e19c2d01
Karlova Lesia <karlovalesa06@gmail.com>
2021-07-20 17:07:09 +0300

    Remove index.php to public directory

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
- define('ROOT', dirname(__FILE__));
- require_once(ROOT.'/Framework/Router/Router.php');
+ const ROOT = '/home/lesia/Desktop/wardrobe';
+ require_once(ROOT . '/Framework/Router/Router.php');
 
  $router = new Router();
  $router->run();
