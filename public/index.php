f179ad497780b090f20816bf9a910c9a57c7f973
Karlova Lesia <karlovalesa06@gmail.com>
2021-07-23 10:54:50 +0300

    Include Session class and call function start()

--- public/index.php
+++ public/index.php
@@ -3,10 +3,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+ require_once '../App/lib/Autoloader.php';
  require_once '../Framework/Router/Router.php';
  require_once '../Framework/Database/Db.php';
+ require_once '../Framework/Session/Session.php';
 
-// session_start();
+
+// spl_autoload_register(['App\lib\Autoloader', 'autoload']);
+
+ $session = new Session();
+ $session->start();
 
  $router = new Router();
  $router->run();
