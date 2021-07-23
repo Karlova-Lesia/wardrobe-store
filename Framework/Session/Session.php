5b4af8608d5fd81417cc99fcfb02aea452d01551
Karlova Lesia <karlovalesa06@gmail.com>
2021-07-23 10:55:55 +0300

    Add class Session

--- Framework/Session/Session.php
+++ Framework/Session/Session.php
@@ -3,5 +3,72 @@
 
 class Session
 {
+    public function setName(string $name): void
+    {
+        session_name($name);
+    }
+
+    public function getName(): string
+    {
+        return session_name();
+    }
+
+    public function setId(string $id): void
+    {
+        session_id($id);
+    }
+
+    public function getId(): string
+    {
+        return session_id();
+    }
+
+    public function cookieExists(): bool
+    {
+        return (!empty($_COOKIE));
+    }
+
+    public function sessionExists(): bool
+    {
+        return (!empty($_SESSION));
+    }
+
+    public function start(): bool
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+            return true;
+        }
+    }
+
+    public function destroy(): void
+    {
+        session_destroy();
+    }
+
+    public function setSavePath(string $savePath): void
+    {
+        session_save_path($savePath);
+    }
+
+    public function set(string $key, string $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get(string $key): string
+    {
+       return $_SESSION[$key];
+    }
+
+    public function contains(string $key): bool
+    {
+        return (isset($_SESSION[$key]));
+    }
+
+    public function delete(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
 
 }
