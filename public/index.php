201bc5edeaf15bc7bb94d8d9adc09b0f2f0a365d
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-14 20:50:31 +0300

    Add composer autoload

--- public/index.php
+++ public/index.php
@@ -3,10 +3,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
- require_once '../App/lib/Autoloader.php';
- require_once '../Framework/Router/Router.php';
- require_once '../Framework/Database/Db.php';
- require_once '../Framework/Session/Session.php';
+ require_once '../vendor/autoload.php';
+
 
  $session = new Session();
  $session->start();
