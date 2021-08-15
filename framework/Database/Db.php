95c22102324abbea9c895cfb88e6de783d1d2545
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-15 22:28:28 +0300

    Delete variable db

--- framework/Database/Db.php
+++ framework/Database/Db.php
@@ -4,14 +4,12 @@ namespace Framework\Database;
 
 class Db
 {
-    public static function getConnection()
+    public static function getConnection(): object
     {
-        $params = include '../App/config/dbParams.php';
+        $params = include '../app/config/dbParams.php';
 
         $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $db = new PDO($dsn, $params['user'], $params['password']);
-
-        return $db;
+        return new PDO($dsn, $params['user'], $params['password']);
     }
 }
 
