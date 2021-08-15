4cfece8028091f1cf8e86f2f1258bf1d702692d6
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-15 22:27:47 +0300

    Add new routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,18 +1,9 @@
 <?php
 
 return [
-    'dresses/([0-9]+)' => 'dresses/oneDress/$1',
-    'dresses' => 'dresses/list', //actionList in DressesController
-    'blouses/([0-9]+)' => 'blouses/oneBlouse/$1',
-    'blouses' => 'blouses/list',
-    'tops/([0-9]+)' => 'tops/oneTop/$1',
-    'tops' => 'tops/list',
-    't-shirts/([0-9]+)' => 't-shirts/oneT-shirt/$1',
-    't-shirts' => 't-shirts/list',
-    'shorts/([0-9]+)' => 'shorts/oneShort/$1',
-    'shorts' => 'shorts/list',
-    'skirts/([0-9]+)' => 'skirts/oneSkirt/$1',
-    'skirts' => 'skirts/list',
-    'jeans/([0-9]+)' => 'jeans/oneJeans/$1',
-    'jeans' => 'jeans/list'
+    '/products/list/([a-z]+)' => 'products/list/$1', //actionList in ProductsController
+    '/products/([a-z]+)' => 'products/oneCategoryList/$1',
+    '/products/([a-z]+)/([0-9]+)' => 'products/oneProduct/$1/$2',
+    '/authorizarion' => 'authorization/index',
+    '/registration' => 'registration/index',
 ];
