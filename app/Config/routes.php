2ef804da74d2f923162011cafda443b40cd76632
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-16 13:15:11 +0300

    Add initialization controller

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,9 +1,1 @@
 <?php
-
-return [
-    '/products/list/([a-z]+)' => 'products/list/$1', //actionList in ProductsController
-    '/products/([a-z]+)' => 'products/oneCategoryList/$1',
-    '/products/([a-z]+)/([0-9]+)' => 'products/oneProduct/$1/$2',
-    '/authorizarion' => 'authorization/index',
-    '/registration' => 'registration/index',
-];
--- public/index.php
+++ public/index.php
@@ -9,3 +9,10 @@ error_reporting(E_ALL);
 
  $router = new Router();
  $router->run();
+
+ $controllerName = $router->getControllerName();
+
+if (class_exists($controllerName)) {
+    $controllerObject = new $controllerName();
+    call_user_func_array([$controllerObject, $router->getActionName()], $router->getParams());
+}
