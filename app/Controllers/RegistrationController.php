8441cb122b969f4c2a3e47f474330c4d3c32e9ad
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-16 21:50:18 +0300

    Add controller for registration

--- app/Controllers/RegistrationController.php
+++ app/Controllers/RegistrationController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controllers;
 
+use Framework\Helpers\ChangePages;
+
 class RegistrationController
 {
+    public static function index(): void
+    {
+        ChangePages::render('registrationPage');
+    }
 
 }
