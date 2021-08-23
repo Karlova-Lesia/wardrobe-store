1c57ed7040ef2406c56333fc7ba967a82ba2a6ed
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-23 14:58:00 +0300

    Add render

--- framework/Core/View.php
+++ framework/Core/View.php
@@ -2,7 +2,25 @@
 
 namespace Framework\Core;
 
+use App\Config\Config;
+
 class View
 {
+    public static function render(string $templateName, array $vars): string
+    {
+        ob_start();
+        extract($vars);
 
+        try {
+            $file = Config::getConfig()['templates']['path'] . "{$templateName}.php";
+            if (!file_exists($file)) {
+                throw new Exception("Configuration file not found.");
+            }
+            include $file;
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        return ob_get_clean();
+    }
 }
