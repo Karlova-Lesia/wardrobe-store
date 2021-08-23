b51326d46a8b5bb39b6bd1577e92ce51815dad4a
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-23 14:56:59 +0300

    Add styles

--- app/View/Templates/productsPage.php
+++ app/View/Templates/productsPage.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
- <?php
-    include_once "../App/Models/Products.php";
-    ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>authorization</title>
+    <title>Products</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
@@ -13,38 +10,114 @@
         crossorigin="anonymous"
     />
     <style>
-        .blockDresses {
+        .main-div {
+            height: 70px;
+            background: white;
+            white-space: nowrap;
+        }
+        .child-div {
             display: inline-block;
         }
-
+        .name-store {
+            font-family: Chilanka, sans-serif;
+            padding-right: 10%;
+            font-size: 40px;
+            width: 25%;
+        }
+        .content-addr-info {
+            color: dimgrey;
+            font-family: "JetBrains Mono Light", sans-serif;
+            font-size: 15px;
+        }
+        .align-color {
+            text-align: right;
+            color: dimgrey;
+        }
+        .parent-div {
+            height: 80px;
+            background: lightcoral;
+            white-space: nowrap;
+        }
+        .child-div {
+            display: inline-block;
+        }
+        .align-right {
+            text-align: right;
+        }
+        .name-store {
+            color: dimgrey;
+            font-family: Chilanka, sans-serif;
+            padding-right: 10%;
+            font-size: 40px;
+            width: 25%;
+        }
+        .categories {
+            font-size: 20px;
+        }
+        .search-block {
+            width: 40%;
+        }
+        .search {
+            width: 100% !important;
+        }
+        .icon {
+            width: 5%;
+        }
+        li > a {
+            background-color: lightgrey !important;
+        }
+        .wrapper {
+            display: inline-block;
+            width: 100%;
+        }
+        .btn-group {
+            width: 100%;
+        }
+        .btns {
+            height: 60px;
+        }
     </style>
 </head>
 <body>
 <?php
-include_once "../Layouts/commons/header.html";
+include_once "../app/View/Layouts/commons/header.html";
 ?>
 <div class="row">
     <div class="col-md-2"><?php
-        include_once "../Layouts/commons/navigation.html";
+        include_once "../app/View/Layouts/commons/navigation.html";
     ?></div>
-    <div class="blockDresses">
-        <div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-6">
             <?php
-            foreach (getProductsList::$productsList as $product) :?>
-                <div class="col-md-3">
-                    <a href="#"><img src="" alt="текст"></a>
-                    <p><a href="/product/<?php echo $product['id'];?>"></a></p>
-                    <p><?php echo $product['name'];?></p>
-                    <p><?php echo $product['description'];?></p>
-                    <p><?php echo $product['price'];?></p>
-                    <div><?php echo $product['img'];?></div>
-                </div>
-        </div>
+
+            foreach ($productsList as $product) :?>
+                    <div style="margin: 20px;"><img style="width: 600px;" src="<?php echo $product['image'];?>" alt="product">
+                    </div>
+
             <?php endforeach;?>
+    </div>
+    <div class="col-md-2">
+        <?php
+
+        foreach ($productsList as $product) :?>
+            <div style="padding-top: 400px; padding-bottom: 400px; padding-right: 50px;">
+                <?php echo $product['name'];?><br>
+                <div style="color: dimgrey;"> <?php echo $product['description'];?><br>
+                    <?php echo $product['price'];?> грн<br></div>
+                <div class="btn-group-vertical btn-group">
+                <button type="button" class="btn btn-outline-secondary btns">
+          <span style="vertical-align: inherit"
+          ><span style="vertical-align: inherit">Додати в кошик</span></span
+          >
+                </button>
+                </div>
+            </div>
+
+        <?php endforeach;?>
     </div>
 </div>
 <?php
-include_once "../Layouts/commons/footer.html";
+include_once "../app/View/Layouts/commons/footer.html";
 ?>
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
