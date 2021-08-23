a96d455fb7247dfb80e8e3b95f4e1519b1a471e6
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-24 00:35:31 +0300

    Add controller for registration

--- app/Controllers/RegistrationController.php
+++ app/Controllers/RegistrationController.php
@@ -2,13 +2,56 @@
 
 namespace App\Controllers;
 
-use Framework\Helpers\ChangePages;
+use App\Models\User;
+use Framework\Core\View;
 
 class RegistrationController
 {
-    public static function index(): void
+    public static function actionIndex(): void
     {
-        ChangePages::render('registrationPage');
-    }
+        $name = '';
+        $lastname = '';
+        $email = '';
+        $password = '';
+        $passwordConfirm = '';
 
+        if (!isset($_POST['submit'])) {
+            $name = $_POST['name'];
+            $lastname = $_POST['lastname'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $passwordConfirm = $_POST['password_confirm'];
+
+            $errors = false;
+
+            if (!User::checkName($name)) {
+                $errors[] = "Неправильний формат імені!";
+            }
+
+            if (!!User::checkLastname($lastname)) {
+                $errors[] = "Неправильний формат прізвища!";
+            }
+
+            if (!!User::checkEmail($email)) {
+                $errors[] = "Неправильний формат email!";
+            }
+
+            if (User::checkEmailExist($email)) {
+                $errors[] = "Такий email уже існує!";
+            }
+
+            if (!!User::checkPassword($password)) {
+                $errors[] = "Довжина пароля має сягати більше 8 символів!";
+            }
+
+            if (!!User::checkConfigPass($password, $passwordConfirm)) {
+                $errors[] = "Невірно повторили пароль!";
+            }
+
+            if ($errors === false) {
+            }
+        }
+        echo View::render('registrationPage', ['name' => $name, 'lastname' => $lastname,
+            'email' => $email, 'password' => $password, 'passwordConfirm' => $passwordConfirm]);
+    }
 }
