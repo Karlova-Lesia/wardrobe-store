c6a91fc677ed45dbcc660214c548967843ee2d95
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-24 18:47:35 +0300

    Add navigation for page

--- app/View/Layouts/commons/header.php
+++ app/View/Layouts/commons/header.php
@@ -1,9 +1,19 @@
 <div class="container-fluid parent-div">
     <nav class="bd-subnavbar py-2" aria-label="Secondary navigation">
-        <div onClick='location.href="http://wardrobe.com/home"' class="name-store child-div align-right">
+        <div
+                onClick='location.href="http://wardrobe.com/home"'
+                class="name-store child-div align-right"
+        >
             <div>wardrobe</div>
         </div>
-        <button onClick='location.href="http://wardrobe.com/products/list"' style="width: 150px;" type="button" class="btn btn-secondary">Каталог</button>
+        <button
+                onClick='location.href="http://wardrobe.com/products/list"'
+                style="width: 150px"
+                type="button"
+                class="btn btn-secondary"
+        >
+            Каталог
+        </button>
         <div class="child-div search-block align-right">
             <div class="input-group w-50 search">
                 <button type="button" class="btn btn-outline-secondary">
@@ -46,38 +56,72 @@
                 <span class="visually-hidden">Button</span>
             </button>
         </div>
-        <div class="child-div icon align-right">
-            <button onClick='location.href="http://wardrobe.com/cabinet"' type="button" class="btn btn-outline-secondary">
-                <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-person"
-                        viewBox="0 0 16 16"
+        <?php if (\App\Models\User::isGuest()) :?>
+            <div class="child-div icon align-right">
+                <button
+                        onClick='location.href="http://wardrobe.com/user/login"'
+                        type="button"
+                        class="btn btn-outline-secondary"
                 >
-                    <path
-                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
-                    ></path>
-                </svg>
-                <span class="visually-hidden">Button</span>
-            </button>
-        </div>
-        <div class="child-div icon align-right">
-            <button onClick='location.href="http://wardrobe.com/user/login"' type="button" class="btn btn-outline-secondary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
-                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
-                </svg>
-                <span class="visually-hidden">Button</span>
-            </button>
-        </div>
-        <div class="child-div icon align-right">
-            <button onClick='location.href="http://wardrobe.com/home"' type="button" class="btn btn-outline-secondary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" href="/user/logout" class="bi bi-unlock" viewBox="0 0 16 16">
-                    <path d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2zM3 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H3z"/>
-                </svg>
-                <span class="visually-hidden">Button</span>
-            </button>
-        </div>
+                    <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-lock"
+                            viewBox="0 0 16 16"
+                    >
+                        <path
+                                d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"
+                        />
+                    </svg>
+                    <span class="visually-hidden">Button</span>
+                </button>
+            </div>
+        <?php else :?>
+            <div class="child-div icon align-right">
+                <button
+                        onClick='location.href="http://wardrobe.com/cabinet"'
+                        type="button"
+                        class="btn btn-outline-secondary"
+                >
+                    <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-person"
+                            viewBox="0 0 16 16"
+                    >
+                        <path
+                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+                        ></path>
+                    </svg>
+                    <span class="visually-hidden">Button</span>
+                </button>
+            </div>
+            <div class="child-div icon align-right">
+                <button
+                        onClick='location.href="http://wardrobe.com/home"'
+                        type="button"
+                        class="btn btn-outline-secondary"
+                >
+                    <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            href="/user/logout"
+                            class="bi bi-unlock"
+                            viewBox="0 0 16 16"
+                    >
+                        <path
+                                d="M11 1a2 2 0 0 0-2 2v4a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h5V3a3 3 0 0 1 6 0v4a.5.5 0 0 1-1 0V3a2 2 0 0 0-2-2zM3 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1H3z"
+                        />
+                    </svg>
+                    <span class="visually-hidden">Button</span>
+                </button>
+            </div>
+        <?php endif;?>
     </nav>
 </div>
