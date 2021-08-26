e5d7ef26c9d887420f5f4bfa924ae441506f2eaa
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-26 15:18:33 +0300

    Change output data by js

--- app/View/Templates/productsPage.php
+++ app/View/Templates/productsPage.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <title>Products</title>
     <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous"
     />
     <style>
         .main-div {
@@ -85,44 +85,19 @@ include_once "../app/View/Layouts/commons/header.php";
 <div class="row">
     <div class="col-md-2"><?php
         include_once "../app/View/Layouts/commons/navigation.php";
-    ?></div>
+        ?></div>
     <div class="col-md-2"></div>
-    <div class="col-md-6">
-            <?php
-
-            foreach ($productsList as $product) :?>
-                    <div style="margin: 20px;"><img style="width: 600px;" src="<?php echo $product['image'];?>" alt="product">
-                    </div>
-
-            <?php endforeach;?>
-    </div>
-    <div class="col-md-2">
-        <?php
-
-        foreach ($productsList as $product) :?>
-            <div style="padding-top: 300px; padding-bottom: 450px; padding-right: 50px;">
-                <?php echo $product['name'];?><br>
-                <div style="color: dimgrey;"> <?php echo $product['description'];?><br>
-                    <?php echo $product['price'];?> грн<br></div>
-                <div class="btn-group-vertical btn-group">
-                <button type="button" class="btn btn-outline-secondary btns">
-          <span style="vertical-align: inherit"
-          ><span style="vertical-align: inherit">Додати в кошик</span></span
-          >
-                </button>
-                </div>
-            </div>
-
-        <?php endforeach;?>
-    </div>
+    <div class = "col-md-6" id ="img" ></div>
+    <div class="col-md-2" id="desc"></div>
 </div>
 <?php
 include_once "../app/View/Layouts/commons/footer.php";
 ?>
+<script src="/index.js"></script>
 <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
 ></script>
 </body>
 </html>
