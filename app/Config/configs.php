8f2f1632057bd909998cb3d3a2a555a65ed0f729
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-28 22:04:47 +0300

    Return array configuration

--- app/Config/configs.php
+++ app/Config/configs.php
@@ -1,1 +1,14 @@
 <?php
+
+return [
+    'templates' => [
+        'path' => '../app/View/Templates/',
+    ],
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'wardrobe',
+        'user' => 'lesya',
+        'password' => 'Password123#@!',
+    ],
+
+];
