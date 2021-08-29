fb9f8432a042f75b06d0ae98ebee4f1d2cca005c
Karlova Lesia <karlovalesa06@gmail.com>
2021-08-30 02:16:59 +0300

    Add routes for cart

--- framework/Router/routes.php
+++ framework/Router/routes.php
@@ -6,6 +6,8 @@ return [
     'products/OneProduct/([0-9]+)' => 'products/oneProduct/$1',
     'products/list' => 'products/list',
     'products/OneCategory/([a-z]+)' => 'products/oneCategory/$1',
+    'cart' => 'cart/index',
+    'cart/add/([0-9]+)' => 'cart/add/$1',
     'user/login' => 'user/login',
     'user/register' => 'user/register',
     'user/logout' => 'user/logout',
